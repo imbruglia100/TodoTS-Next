6a800f177f293d4260a3eb5f9726ea245a9acdc1
Frank <imbruglia100@gmail.com>
2023-01-03 16:43:44 -0500

    Persistent data... Cool

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,63 +1,89 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Styles from '../styles/index.module.css'
+import createPersistedState from "use-persisted-state";
+
+const useTodoState = createPersistedState<Array<string>>("todo")
+const useDoneState = createPersistedState<Array<string>>("done");
 
 const Index = () => {
-    type Todo = {
-        name: string
-    }
 
-    const [toDoList, setToDoList] = useState<Array<Todo>>([]);
-    const [doneList, setDoneList] = useState<Array<Todo>>([]);
+    const [initialRender, setInitialRender] = useState(false)
+
+    const [toDoList, setToDoList] = useTodoState();
+    const [doneList, setDoneList] = useDoneState();
     const [input, setInput] = useState<string>('')
 
-    const handleClick = () => {
-        setToDoList((prev) => [...prev, {name: input}])
+    const addTodo = () => {
+        setToDoList((prev: Array<string>) => [...prev, input])
         setInput('')
     }
-
-    const handleDone = (e: any) => {
-        console.log(e)
-        setDoneList((prev) => [...prev, { name: e.target.innerHTML }]);
-        setToDoList((prev) => prev.filter( ele => ele.name !== e.target.innerHTML))
+    
+    const moveToDone = (e: any) => {
+        setDoneList((prev: Array<string>) => [...prev, e.target.innerHTML]);
+        setToDoList((prev: Array<string>) =>
+          prev.filter((ele) => ele !== e.target.innerHTML)
+        );
     }
 
     const handleChange = (e: {target: {value: string}}) => {
         setInput(e.target.value)
     }
-  return (
+    
+    const deleteFromList = (e) => {
+        setDoneList((prev: Array<string>) =>
+          prev.filter((ele) => ele !== e.target.innerHTML)
+        );
+    }
+
+    const clear = () => {
+        setDoneList([])
+        setToDoList([])
+    }
+
+    useEffect( () => {
+        setInitialRender(true)
+    }, [])
+
+    if(!initialRender){
+        return null
+    }else{
+
+    return (
     <div className={Styles.wFull}>
-      <h1 className={Styles.textCenter}>Todo List</h1>
-      <div className={Styles.row}>
+        <h1 className={Styles.textCenter}>Todo List</h1>
+        <div className={Styles.row}>
         <ul>
-          <h2>ToDo</h2>
-          {toDoList &&
-            toDoList.map((toDo, i) => {
-              return (
-                <li onClick={handleDone} className={Styles.listItem} key={toDo.name + i}>
-                  {toDo.name}
-                </li>
-                
-              );
-            })}
+            <h2>ToDo</h2>
+            {toDoList &&
+                toDoList.map((toDo, i) => {
+                    return (
+                    <li onClick={moveToDone} className={Styles.toDoListItem} key={toDo + i}>
+                        {toDo}
+                    </li>
+                    
+                    );
+                })}
         </ul>
         <ul>
-          <h2>Done</h2>
-          {doneList &&
+            <h2>Done</h2>
+            {doneList &&
             doneList.map((toDo, i) => {
-              return (
-                <li className={Styles.listItem} key={toDo.name + i}>
-                  {toDo.name}
+                return (
+                <li onClick={deleteFromList} className={Styles.doneListItem} key={toDo + i}>
+                    {toDo}
                 </li>
-              );
+                );
             })}
         </ul>
-      </div>
-      <div>
+        </div>
+        <div>
         <input value={input} onChange={handleChange} />
-        <button onClick={handleClick}>Add</button>
-      </div>
+        <button onClick={addTodo}>Add</button>
+        </div>
+        <button style={{backgroundColor: "red", marginTop: 20}} onClick={clear}>Clear</button>
     </div>
-  );
+    );
+  }
 }
 
 export default Index
